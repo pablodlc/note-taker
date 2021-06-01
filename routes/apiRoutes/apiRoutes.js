e6d997a0621c6e16d7abc6858994007bd0c27401
pablo de la cruz <pablodlc@gmail.com>
2021-05-31 21:33:59 -0700

    refactored to use  instead of

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,42 +1,55 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const filterId = require('../../lib/script');
 // Adding variable `notes` to access my database easily
-const notes = require('../../db/db');
 
 // Installed UUID to give notes ids. Learned about this and much of this code in class today with Cody H. and our office hours sub, Phil Cowan.
 const { v4: uuidv4 } = require('uuid');
 
 // If there's a request for notes in the path, `res.json(notes)` returns the notes to the page.
-router.get("/notes", (req, res) => {
-    res.json(notes);
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err
+        res.json(JSON.parse(data));
+    });
 });
 
-router.post('/notes', (req, res) => {
-    // `newNote` is created to hold the user's input
-    const newNote = req.body;
-    // Using UUID to give the note a unique id, making it easier to delete
-    newNote.id = uuidv4();
-    // Here we're pushing `newNote` into `notes`, aka db.json.
-    notes.push(newNote);
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
-        if (err) throw err
-    })
-    res.json(notes)
+
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    let notes = JSON.parse(data);
+    router.post("/notes", (req, res) => {
+        let newNote = req.body;
+        newNote.id = uuidv4();
+        notes.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+            if (err) throw err
+        })
+        res.json(newNote)
+    });
 });
 
-router.delete('/notes/:id', (req, res) => {
-    // let
-    // Using a variable to say "id" easier
-    let id = req.params.id;
-    // This is a new array that filters through db.json. If the id of the note doesn't match the deleted id, it's added to `notesToSave`. A matching id is ignored, so effectively deleted.
-    let notesToSave = notes.filter((note) => {
-        return note.id !== id;
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    let notes = JSON.parse(data);
+    router.delete('/notes/:id', (req, res) => {
+        const postDeleteArray = filterId(req.params.id, notes);
+        res.send(postDeleteArray)
     })
-    fs.writeFile('./db/db.json', JSON.stringify(notesToSave), (err, data) => {
-        if (err) throw err
-    })
-    res.json(notes)
 })
+
+// router.delete('/notes/:id', (req, res) => {
+//     // Using a variable to say "id" easier
+//     let id = req.params.id;
+//     // This is a new array that filters through db.json. If the id of the note doesn't match the deleted id, it's added to `notesToSave`. A matching id is ignored, so effectively deleted.
+//     let notesToSave = notes.filter((note) => {
+//         return note.id !== id;
+//     })
+//     let currentNotes = require('../../db/db');
+//     fs.writeFile('./db/db.json', JSON.stringify(notesToSave), (err, data) => {
+//         if (err) throw err
+//     })
+//     res.json(currentNotes)
+// })
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+
 // Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
